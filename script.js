eedccf9b07f9a117213082533468a2de0e567ca6
FelixT <felixtekhada@gmail.com>
2022-10-29 23:07:28 +0300

    git test

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-console.log('hi');
+// console.log('hi');
+// const myStr = ""; // Change this line
+// const sampleStr = "I am a \"double quoted\" string inside \"double quotes\"."
+// console.log(sampleStr);
+// console.log('I am a "double quoted" string inside "double quotes".');
+// const myStr = "FirstLine \n \t \\SecondLine \nThirdLine"
+// console.log(myStr);
+// const functionWithArgs =  (a,b) =>{
+//     return a+b;
+//     }
+//      console.log(functionWithArgs(1,2));
+// function trueOrFalse(wasThatTrue) {
+    // Only change code below this line
+//   return wasThatTrue?'Yes, that was true':'No, that was false';
+    // Only change code above this line
+//   }
+// console.log(trueOrFalse(true));
 
